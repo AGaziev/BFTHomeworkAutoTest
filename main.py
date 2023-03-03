3d329417e64646656e26e0994fd20c47de169f0c
Alan Gaziev <alan.gaziev27@gmail.com>
2023-03-03 21:05:51 +0200

    finally... three tests

--- main.py
+++ main.py
@@ -1,30 +1,101 @@
 import allure
 import time
 
-#from allure_commons.types import At...
+# from allure_commons.types import At...
 from selenium import webdriver
-from driverRep import DriverPath
+# from driverRep import DriverPath
 from stepper import Stepper
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-driverPath = DriverPath.getDriverPath()
+driverPath = r'drivers\driver.exe'  # DriverPath.getDriverPath()
+
 
 class TestGithubFeatures:
+    login = 'adminGithuba'
+    password = 'hardpass999'
+    profileUsernameToFind = 'VoiceDD'
+    testRepo = 'BFTHomeworkAutoTest'
+
     def setup(self):
         self.driver = webdriver.Edge(driverPath)
+        self.driver.maximize_window()
         self.stepper = Stepper(self.driver)
 
     def teardown(self):
         self.driver.quit()
 
+    @allure.step('Waiting for DOM loaded')
+    def wait(self, sec=1):
+        time.sleep(sec)
+
+    @allure.feature("Test Github Features")
     @allure.story('Authentication to github')
-    def login(self):
+    def test_login(self):
+        self.stepper.open_page('https://github.com/login')
+        self.stepper.send_keys('login_field',
+                               By.ID, self.login, name='login field')
+        self.stepper.send_keys('password',
+                               By.ID, self.password, name='password field')
+        self.stepper.click('/html/body/div[1]/div[3]/main/div/div[4]/form/div/input[11]',
+                           By.XPATH, name='Sign in button')
+        with allure.step('Check current URL is main page'):
+            assert self.driver.current_url == 'https://github.com/'
+        self.checkUser(self.login)
+        self.driver.refresh()  # bc need to close user panel features menu (otherwise intercepted)
 
+    @allure.step('Check user {username} on main page')
+    def checkUser(self, username):
+        self.stepper.click('/html/body/div[1]/div[1]/header/div[7]/details/summary',
+                           By.XPATH, 'profile features container')
+        actualUsername = self.stepper.getAttribute(
+            '/html/body/div[1]/div[1]/header/div[7]/details/details-menu/div[1]/a',
+            By.XPATH, 'text', name='textbox contains username')
+        actualUsername = actualUsername.removeprefix('Signed in as ')
+        assert actualUsername == username
 
-    @allure.feature('GitHub Features Test')
-    def test_github_features(self):
-        self.driver.get('https://github.com/login')
-        self.login()
+    @allure.feature("Test Github Features")
+    @allure.story('Finding user profile by search')
+    def test_find_user(self):
+        with allure.step('Login'):
+            self.test_login()
+        with allure.step('Start search by username'):
+            self.stepper.send_keys('/html/body/div[1]/div[1]/header/div[3]/div/div/form/label/input[1]',
+                                   By.XPATH, self.profileUsernameToFind, 'search')
+            self.stepper.click('jump-to-suggestion-search-global',
+                               By.ID, 'Search All Github')
+            self.stepper.click('/html/body/div[1]/div[6]/main/div/div[2]/nav[1]/a[10]',
+                               By.XPATH, 'User Filter')
+            self.stepper.click(f'//*[@id="user_search_results"]/div//a[contains(em, "{self.profileUsernameToFind}")]',
+                               By.XPATH, "User Card in results")
+        with allure.step("Star a test repository"):
+            self.stepper.click(
+                f'//*[@id="user-profile-frame"]/div/div[1]/div/ol//li//a[contains(span,"{self.testRepo}")]',
+                By.XPATH, "test repository card")
+            starCountBefore = int(self.stepper.getAttribute(
+                '//*[@id="repo-content-pjax-container"]/div/div/div[2]/div[2]/div/div[1]/div/div[2]/a/strong',
+                By.XPATH, 'innerText', 'star count'))
+            self.stepper.click(
+                '/html/body/div[1]/div[6]/div/main/div/div[1]/ul/li[3]/div/div[2]/form/button',
+                By.XPATH, 'star button')
+            time.sleep(1)  # wait until star info refreshing on server
+            self.driver.refresh()
+            starCountAfter = int(self.stepper.getAttribute(
+                '//*[@id="repo-content-pjax-container"]/div/div/div[2]/div[2]/div/div[1]/div/div[2]/a/strong',
+                By.XPATH, 'innerText', 'star count'))
+        with allure.step("Star comparison test"):
+            assert starCountAfter - starCountBefore == 1
 
-        self.findUser('VoiceDD')
-        self.setStarToRep('')
+    def test_logout(self):
+        with allure.step('Login'):
+            self.test_login()
+        with allure.step('Logout'):
+            self.stepper.click('/html/body/div[1]/div[1]/header/div[7]/details/summary',
+                               By.XPATH, 'profile features container')
+            self.stepper.click('/html/body/div[1]/div[1]/header/div[7]/details/details-menu/form/button',
+                               By.XPATH, 'Sign out button')
+        with allure.step('Check logout completed'):
+            signIn = self.stepper.getAttribute('/html/body/div[1]/div[1]/header/div/div[2]/div/div/div[2]/a',
+                                               By.XPATH, 'innerText', 'Sign in button')
+            assert signIn == 'Sign in'
--- stepper.py
+++ stepper.py
@@ -1,10 +1,16 @@
+import time
+
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 import allure
+from functools import wraps
 
-class Stepper: # idk how to name this class
+
+class Stepper:  # idk how to name this class
     def __init__(self, driver: WebDriver):
         self.driver = driver
 
@@ -12,22 +18,25 @@ class Stepper: # idk how to name this class
     def open_page(self, url):
         self.driver.get(url)
 
-    @allure.step("Send text {text} to {elementId}")
-    def send_keys(self, elementInfo, text, by):
-        element, elementId = self.getElement(by, elementInfo)
+    @allure.step("Send {text} to {name}")
+    def send_keys(self, elementInfo, by, text, name='element'):
+        element, elementId = self.getElement(elementInfo, by)
         element.send_keys(text)
 
-    @allure.step("Click {elementId}")
-    def click(self, elementInfo, by):
-        element, elementId = self.getElement(by, elementInfo)
+    @allure.step("Click {name}")
+    def click(self, elementInfo, by, name='element'):
+        element, elementId = self.getElement(elementInfo, by)
         element.click()
 
-    @allure.step("Get {attributeName} from {elementId}")
-    def getAttribute(self, elementInfo, attributeName, by):
-        element, elementId = self.getElement(by, elementInfo)
+    @allure.step("Get {attributeName} from {name}")
+    def getAttribute(self, elementInfo, by, attributeName, name='element'):
+        element, elementId = self.getElement(elementInfo, by)
         return element.get_attribute(attributeName)
 
-    def getElement(self, by, elementInfo):
-        element = self.driver.find_element(by, elementInfo)
-        elementId = element.get_attribute('id')
+    def getElement(self, elementInfo, by):
+        element = WebDriverWait(self.driver,10).until(EC.presence_of_element_located((by,elementInfo)))
+        try:
+            elementId = element.id
+        except:
+            elementId = 'NoneId'
         return element, elementId
